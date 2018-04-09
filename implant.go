2f94827631520b9ae38588796edb7eea8aa03b06
Steve Kemp <steve@steve.org.uk>
2018-04-09 11:43:13 +0300

    Allow excluding files from the input directory via regexp.
    This is useful if your data-tree is stored under revision
    control and you wish to exclude files matching ".git/",
    ".hg/" or similar.

--- implant.go
+++ implant.go
@@ -20,6 +20,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"text/template"
 )
 
@@ -29,6 +30,7 @@ import (
 var ConfigOptions struct {
 	Input   string
 	Output  string
+	Exclude string
 	Format  bool
 	Verbose bool
 }
@@ -129,21 +131,73 @@ func getResources() []string {
 `
 
 //
-// This is the structure of files we've found, which we'll use to
-// populate the output file.
+// This is the structure of resources we've found, which we'll embed in the
+// output template.
 //
 type Resource struct {
 	Filename string
 	Contents string
 }
 
+//
 // Write gzipped data to a Writer
+//
 func gzipWrite(w io.Writer, data []byte) error {
 	// Write gzipped data to the client
 	gw, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
 	defer gw.Close()
 	gw.Write(data)
 	return err
+}
+
+//
+// Should we include this file/resource?
+//
+// We skip directories, and we might skip by regular expression too
+//
+func ShouldInclude(path string) bool {
+
+	//
+	// Examine the file.
+	//
+	stat, err := os.Stat(path)
+	if err != nil {
+		if ConfigOptions.Verbose {
+			fmt.Printf("Failed to stat file : %s\n", path)
+		}
+		return false
+	}
+
+	//
+	// Is it a regular file?
+	//
+	mode := stat.Mode()
+	if !mode.IsRegular() {
+		//
+		// If it isn't we shouldn't include it.
+		//
+		return false
+	}
+
+	//
+	// If we have a regular expression then test against it.
+	//
+	if ConfigOptions.Exclude != "" {
+
+		//
+		// If the regexp matches then we must EXCLUDE
+		// the file.
+		//
+		match, _ := regexp.MatchString(ConfigOptions.Exclude, path)
+		if match {
+			return false
+		}
+	}
+
+	//
+	// OK the file wasn't excluded, so we'll add it.
+	//
+	return true
 }
 
 //
@@ -160,21 +214,19 @@ func findFiles(input string) ([]Resource, error) {
 
 	// Recursively look for files to add.
 	err := filepath.Walk(input, func(path string, f os.FileInfo, err error) error {
-		//
-		// We only care about plain-files
-		//
-		// So we skip directories, symlinks, etc.
-		//
-		stat, err := os.Stat(path)
-		if err != nil {
-			return err
+		if ConfigOptions.Verbose {
+			fmt.Printf("Found file: %s\n", path)
 		}
-
-		switch mode := stat.Mode(); {
-
-		case mode.IsRegular():
-
+		if ShouldInclude(path) {
+			if ConfigOptions.Verbose {
+				fmt.Printf("Including file: %s\n", path)
+			}
 			fileList = append(fileList, path)
+		} else {
+			if ConfigOptions.Verbose {
+				fmt.Printf("Excluding file: %s\n", path)
+			}
+
 		}
 		return nil
 	})
@@ -254,6 +306,7 @@ func main() {
 	//
 	// The command-line flags we support
 	//
+	flag.StringVar(&ConfigOptions.Exclude, "exclude", "", "A regular expression of files to ignore, for example '.git'.")
 	flag.StringVar(&ConfigOptions.Input, "input", "data/", "The directory to read from.")
 	flag.StringVar(&ConfigOptions.Output, "output", "static.go", "The output file to generate.")
 	flag.BoolVar(&ConfigOptions.Verbose, "verbose", false, "Should we be verbose.")
@@ -309,7 +362,10 @@ func main() {
 	// Show how many files we found.
 	//
 	if ConfigOptions.Verbose {
-		fmt.Printf("Found %d files.\n", len(files))
+		fmt.Printf("Populating static.go with the following files.\n")
+		for _, ent := range files {
+			fmt.Printf("\t%s\n", ent.Filename)
+		}
 	}
 
 	//
