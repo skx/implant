f472ab3a7728ea4cb98f44892331b28e6e84416f
Steve Kemp <steve@steve.org.uk>
2018-04-09 20:51:39 +0300

    Simple test-cases of our own static-data

---
+++ static_test.go
@@ -0,0 +1,56 @@
+//
+// Simple testing of our embedded resource.
+//
+//
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+//
+// Test that we have one embedded resource.
+//
+func TestResourceCount(t *testing.T) {
+	out := getResources()
+	if len(out) != 1 {
+		t.Errorf("We expected one resource but found %d.", len(out))
+	}
+}
+
+//
+// Test that our resource is identical to our static-file.
+//
+func TestResourceMatches(t *testing.T) {
+
+	//
+	// Get the data from our embedded copy
+	//
+	data, err := getResource("data/static.tmpl")
+	if err != nil {
+		t.Errorf("Loading our resource failed.")
+	}
+
+	//
+	// Get the data from our master-copy.
+	//
+	master, err := ioutil.ReadFile("data/static.tmpl")
+	if err != nil {
+		t.Errorf("Loading our master-resource failed.")
+	}
+
+	//
+	// Test the lengths match
+	//
+	if len(master) != len(data) {
+		t.Errorf("Embedded and real resources have different sizes.")
+	}
+
+	//
+	// Test the data-matches
+	//
+	if string(master) != string(data) {
+		t.Errorf("Embedded and real resources have different content.")
+	}
+}
